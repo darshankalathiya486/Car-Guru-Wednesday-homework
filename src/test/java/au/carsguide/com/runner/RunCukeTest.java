ad89b14289f53cd6811aadb368ffce62797e7348
darshankalathiya486 <darshankalathiya486@gmail.com>
2023-12-22 17:24:56 +0000

    car Guru

---
+++ src/test/java/au/carsguide/com/runner/RunCukeTest.java
@@ -0,0 +1,19 @@
+package au.carsguide.com.runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "au/carsguide/com/steps",
+        plugin = {"html:target/cucumber-reports/cucumber.html",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+        tags = "@regression"
+
+)
+public class RunCukeTest extends AbstractTestNGCucumberTests {
+
+}
+
+
---
+++ src/test/java/au/carsguide/com/steps/FindDealersSteps.java
@@ -0,0 +1,25 @@
+package au.carsguide.com.steps;
+
+
+import au.carsguide.com.pages.CarDealersPage;
+import au.carsguide.com.pages.HomePage;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import org.testng.Assert;
+
+public class FindDealersSteps {
+    @And("I click Find a Dealer")
+    public void iClickFindADealer() {
+        new HomePage().clickOnFindADealerOptionInBuyAndSellTopMenu();
+    }
+
+    @Then("I navigate to car-dealers page")
+    public void iNavigateToCarDealersPage() {
+        Assert.assertEquals(new CarDealersPage().getWelcomeText(), "Find a Car Dealership Near You");
+    }
+
+    @And("I should see the dealer names {string} are display on page")
+    public void iShouldSeeTheDealerNamesAreDisplayOnPage(String dealerName) {
+        Assert.assertEquals(new CarDealersPage().isCarDealerNameAvailable(dealerName), dealerName);
+    }
+}
